a64a5f2d48cf21f71b62471feb453e3c6710bc77
Adoni-mafi <aronamare01@gmail.com>
2026-06-05 11:50:02 +0300

    i added loop to contunue the game if the user need otherwise terminated

--- RPS_GAME.py
+++ RPS_GAME.py
@@ -12,17 +12,26 @@
 '''
 import random
 choices = ('r','p','s')
-user_input = input('ROCK, PAPER OR SECISSER (r,p,s): ').lower()
-if user_input not in  choices :
-    print("Invalid Enter")  
-computer_choice = random.choice(choices).lower()
-print("Computer is : " ,computer_choice)
-print("I Choose : ", user_input)
-if user_input == computer_choice:
-    print ("Let's Try Another Game")
-elif (user_input == 'r' and computer_choice =='s')or \
+while True:
+    user_input = input('ROCK, PAPER OR SECISSER (r,p,s): ').lower()
+    if user_input not in  choices :
+        print("Invalid Enter") 
+        continue
+ 
+    computer_choice = random.choice(choices).lower()
+    print("Computer is : " ,computer_choice)
+    print("I Choose : ", user_input)
+    if user_input == computer_choice:
+       print ("Let's Try Another Game")
+    elif (user_input == 'r' and computer_choice =='s')or \
       (user_input == 's' and computer_choice =='p') or \
       (user_input == 'p' and computer_choice =='r') :
-    print ('You Win!!')
-else:
-    print('You Loss!!')
+         print ('You Win!!')
+    else:
+        print('You Loss!!')
+    should_continue = input('continue? (y/n): ').lower()
+    if should_continue =='n':
+        break
+
+
+    
